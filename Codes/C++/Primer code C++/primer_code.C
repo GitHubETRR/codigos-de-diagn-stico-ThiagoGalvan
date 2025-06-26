e224f6db6ba1d97fb5eb020bb07f6dd6691ee057
ThiagoGalvan <galvanabbondanzathiago@gmail.com>
2025-06-26 14:42:23 -0300

    chamba 26/6

---
+++ Codes/C++/Primer code C++/primer_code.C
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int numero;
+    cout << "Ingrese un valor por favor\n";
+    cin >> numero;
+    //Se concatenan y muestran los valores por pantalla con cout<<
+    cout << "El valor de numero es " << numero << "\n";
+    return 0;
+}
---
+++ hola.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+#define CHONY 1
+#define CHIKON 2
+#define BERRETA 0
+class camara{
+    
+    public:
+        int marca;
+    void tomar_foto(void){
+        cout<<"Sonria!!!"<<endl;
+    }
+    void imprimir_marca(void){
+        if(marca==CHONY){
+            cout<<"Chony"<<endl;
+        }else if(marca==CHIKON){
+            cout<<"Chikon"<<endl;
+        }else{
+            cout<<"Berreta sin marca"<<endl;
+        }
+    }
+    camara(int marcax){
+        cout<<"Camara creada"<<endl;
+        marca=marcax;
+    }
+    camara(void){
+        cout<<"Camara creada"<<endl;
+        marca=BERRETA;
+    }
+
+    ~camara(void){
+        cout<<"Camara borrada"<<endl;
+    }
+};
+
+int main (void){
+    camara * cam=new(camara);
+    cam->tomar_foto();
+    cam->imprimir_marca();
+    delete(cam);
+    cout<<"Cerrando..."<<endl;
+    return 0;
+}
