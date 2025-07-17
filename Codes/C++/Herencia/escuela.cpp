b1c403ef32a9d34b264ff52dc2fbad5ffdc8a197
ThiagoGalvan <galvanabbondanzathiago@gmail.com>
2025-07-17 12:04:34 -0300

    avances clase 17/7

--- Codes/C++/Herencia/escuela.cpp
+++ Codes/C++/Herencia/escuela.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-typdef enum{
+typedef enum{
     SALIR,
     CREAR_USUARIO,
     CREAR_ALUMNO,
@@ -24,7 +24,41 @@ persona :: persona(string n, string a, int d, int e) {
     apellido = a;
     dni = d;
     edad = e;
-    cout << "Hola, soy " << n << " " << a << " \nMi dni es: " << d << " y mi edad es " << e << endl;
+    cout << "\nHola, soy " << n << " " << a << " \nMi dni es: " << d << " y mi edad es " << e << endl;
+}
+
+class alumno : public persona {
+    private:
+        int numero_alumno;
+        int pruebas_aprobadas;
+        int pruebas_desaprobadas;
+        float promedio;
+        int asistencia;
+
+    public:
+    void setCodigoAlumno(int);
+    void setResultadoPrueba(int);
+    void cargarAsistencia(bool);
+    void calcularPromedio();//la voy a hacer despues para poner archivos y para guardar las notas
+
+};
+
+void alumno::setCodigoAlumno(int num) {
+    numero_alumno = num;
+}
+
+void alumno::setResultadoPrueba(int resultado) {
+    if (resultado => 7) {
+        pruebas_aprobadas++;
+    } else {
+        pruebas_desaprobadas++;
+    }
+}
+
+void alumno::cargarAsistencia(bool asistencia) {
+    if (asistencia==1) {
+        asistencia++;
+    }
 }
 
 int main(){
@@ -36,19 +70,40 @@ int main(){
     cin >> opcion;
 
     switch (opcion){
-    case CREAR_USUARIO:
-        persona p1("Juan", "Perez", 12345678, 30);
-        break;
-    case CREAR_ALUMNO:
-        cout <<"Esta funcion no esta definida" << endl;
-        break;
+    case CREAR_USUARIO:{
+        
+        string nombre;
+        string apellido;
+        int dni;
+        int edad;
 
-    case CREAR_PROFESOR:
+        cout << "Ingrese el nombre: ";
+        cin >> nombre;
+        cout << "Ingrese el apellido: ";
+        cin >> apellido;
+        cout << "Ingrese el DNI: ";
+        cin >> dni;
+        cout << "Ingrese la edad: ";
+        cin >> edad;
+
+        persona p1(nombre, apellido,dni, edad);
+        break;
+    }
+
+    case CREAR_ALUMNO:{
+
         cout <<"Esta funcion no esta definida" << endl;
         break;
-    case SALIR:
+    }
+
+    case CREAR_PROFESOR:{
+        cout <<"Esta funcion no esta definida" << endl;
+        break;
+    }
+    case SALIR:{
         cout << "Hasta luego" << endl;
         break;
+    }
     default:
         break;
     }
