baec40d294afb12e1274cbb64aa91bdc9b740d48
ThiagoGalvan <galvanabbondanzathiago@gmail.com>
2025-08-27 22:58:32 -0300

    agregue constructores, metodos a ambas clases. Y mejore un poquito el menu

--- Codes/C++/Herencia/escuela.cpp
+++ Codes/C++/Herencia/escuela.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 typedef enum{
-    SALIR,
-    CREAR_USUARIO,
-    CREAR_ALUMNO,
-    CREAR_PROFESOR
+    CREAR_ALUMNO = 1,
+    CREAR_PROFESOR,
+    SALIR
 }opcion_t;
 
+//Clase persona
 class persona {
     private:
         string nombre;
@@ -18,7 +18,7 @@ class persona {
     public: 
         persona (string, string, int, int); 
 };
-
+//Constructor de la clase persona
 persona :: persona(string n, string a, int d, int e) {
     nombre = n;
     apellido = a;
@@ -27,6 +27,9 @@ persona :: persona(string n, string a, int d, int e) {
     cout << "\nHola, soy " << n << " " << a << " \nMi dni es: " << d << " y mi edad es " << e << endl;
 }
 
+
+
+//Clase alumno con la herencia de persona
 class alumno : public persona {
     private:
         int numero_alumno;
@@ -40,38 +43,108 @@ class alumno : public persona {
     void setResultadoPrueba(int);
     void cargarAsistencia(bool);
     void calcularPromedio();//la voy a hacer despues para poner archivos y para guardar las notas
+    alumno(string, string, int, int, int);
 
 };
 
+//Metodos de la clase alumno
 void alumno::setCodigoAlumno(int num) {
     numero_alumno = num;
 }
-
 void alumno::setResultadoPrueba(int resultado) {
-    if (resultado => 7) {
+    while (resultado < 0 || resultado > 10){
+        cout << "Error, ingrese un resultado valido (0-10): ";
+        cin >> resultado;
+    }
+    if (resultado >= 7){
         pruebas_aprobadas++;
     } else {
         pruebas_desaprobadas++;
     }
 }
-
-void alumno::cargarAsistencia(bool asistencia) {
-    if (asistencia==1) {
+void alumno::cargarAsistencia(bool presente) {
+    if (presente==1) {
         asistencia++;
     }
 }
+//Constructor de la clase alumno
+alumno::alumno(string n, string a, int d, int e, int num) : persona(n,a,d,e) {
+    numero_alumno = num;
+    pruebas_aprobadas = 0;
+    pruebas_desaprobadas = 0;
+    promedio = 0;
+    asistencia = 0;
+}
+
+
+
+
+//Clase profesor con la herencia de persona
+class profesor : public persona {
+    private:
+        int codigo_profesor;
+        int antiguedad;
+        string materia;
+
+    public:
+        void setCodigoProfesor(int);
+        void setAntiguedad(int);
+        void setMateria(string);
+        profesor(string, string, int, int, int, string);
+
+};
+
+//Metodos de la clase profesor
+void profesor::setCodigoProfesor(int cod) {
+    codigo_profesor = cod;
+}
+void profesor::setAntiguedad(int antig) {
+    antiguedad = antig;
+}
+void profesor::setMateria(string mat) {
+    materia = mat;
+}
+
+//Constructor de la clase profesor
+profesor::profesor(string n, string a, int d, int e, int cod, string mat) : persona(n,a,d,e) {
+    codigo_profesor = cod;
+    antiguedad = 0;
+    materia = mat;
+}
+
 
 int main(){
     int opcion;
     cout << "Bienvenido al sistema de gestion escolar" << endl;
-    cout << "1. Crear un usuario" << endl;
-    cout << "2. Crear un alumno" << endl;
-    cout << "3. Crear un profesor" << endl;
+    cout << "1. Crear un alumno" << endl;
+    cout << "2. Crear un profesor" << endl;
+    cout << "3. Salir" << endl;
+    cout << "Ingrese una opcion: ";
     cin >> opcion;
 
     switch (opcion){
-    case CREAR_USUARIO:{
+    case CREAR_ALUMNO:{
+        string nombre;
+        string apellido;
+        int dni;
+        int edad;
+        int numero_alumno;
+
+        cout << "Ingrese el nombre: ";
+        cin >> nombre;
+        cout << "Ingrese el apellido: ";
+        cin >> apellido;
+        cout << "Ingrese el DNI: ";
+        cin >> dni;
+        cout << "Ingrese la edad: ";
+        cin >> edad;
         
+        alumno a1(nombre, apellido,dni,edad, numero_alumno);
+        break;
+    }
+
+    case CREAR_PROFESOR:{
+
         string nombre;
         string apellido;
         int dni;
@@ -85,21 +158,10 @@ int main(){
         cin >> dni;
         cout << "Ingrese la edad: ";
         cin >> edad;
-
-        persona p1(nombre, apellido,dni, edad);
+        
         break;
     }
 
-    case CREAR_ALUMNO:{
-
-        cout <<"Esta funcion no esta definida" << endl;
-        break;
-    }
-
-    case CREAR_PROFESOR:{
-        cout <<"Esta funcion no esta definida" << endl;
-        break;
-    }
     case SALIR:{
         cout << "Hasta luego" << endl;
         break;
